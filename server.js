0ad852ef2c23cf1de7d613fa37c5a4d48e69d1ea
Jmichael96-edge <jeffrey.vanhorn@edgeofs.com>
2021-09-20 15:55:00 -0500

    added process

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const path = require('path');
 const cors = require('cors');
 const morgan = require('morgan');
